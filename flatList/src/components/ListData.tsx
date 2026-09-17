f684e34e0593c0fafd01f18639a4be371a99753f
Esther Okiki <estherokiki15@gmail.com>
2026-09-17 12:39:37 +0100

    feat: initialize flatList project with essential files and components

---
+++ flatList/src/components/ListData.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ListData = () => {
+  return (
+    <View>
+      <Text>ListData</Text>
+    </View>
+  );
+};
+
+export default ListData;
